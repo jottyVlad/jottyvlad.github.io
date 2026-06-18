c57903df177d874504aa8e5c66df6d47ecca9e59
Vladislav <v.bardatskiy@gmail.com>
2026-06-18 16:31:01 +0300

    updates

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
-import {loadJSON} from "/js/index.js";
+// import {loadJSON} from "/js/index.js";
 
-let about_section = document.getElementById("about-me-inner");
+// let about_section = document.getElementById("about-me-inner");
 
-loadJSON('/js/json/index.json', response => {
-    let json = JSON.parse(response);
-    about_section.innerText = json["about"];
-});
+// loadJSON('/js/json/index.json', response => {
+//     let json = JSON.parse(response);
+//     about_section.innerText = json["about"];
+// });
--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,23 @@
-import {loadJSON} from "/js/index.js";
+// import {loadJSON} from "/js/index.js";
 
-let projectsListHtml = document.getElementsByClassName("projects-list")[0]
+// let projectsListHtml = document.getElementsByClassName("projects-list")[0]
 
-loadJSON("/js/json/projects.json", response => {
-    let json = JSON.parse(response);
-    json.forEach(elem => {
-        let newDiv = document.createElement('div');
-        newDiv.className = "row project";
+// loadJSON("/js/json/projects.json", response => {
+//     let json = JSON.parse(response);
+//     json.forEach(elem => {
+//         let newDiv = document.createElement('div');
+//         newDiv.className = "row project";
 
-        let projectNameDiv = document.createElement('div');
-        projectNameDiv.className = "row project-name";
-        projectNameDiv.innerText = elem["name"];
-        newDiv.appendChild(projectNameDiv);
+//         let projectNameDiv = document.createElement('div');
+//         projectNameDiv.className = "row project-name";
+//         projectNameDiv.innerText = elem["name"];
+//         newDiv.appendChild(projectNameDiv);
 
-        let projectDescriptionDiv = document.createElement('div');
-        projectDescriptionDiv.className = "row project-description";
-        projectDescriptionDiv.innerText = elem["description"];
-        newDiv.appendChild(projectDescriptionDiv);
+//         let projectDescriptionDiv = document.createElement('div');
+//         projectDescriptionDiv.className = "row project-description";
+//         projectDescriptionDiv.innerText = elem["description"];
+//         newDiv.appendChild(projectDescriptionDiv);
 
-        projectsListHtml.appendChild(newDiv);
-    })
-})
+//         projectsListHtml.appendChild(newDiv);
+//     })
+// })
